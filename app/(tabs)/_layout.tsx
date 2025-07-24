962cb51725529630e784a33319f9a7740d78aeea
d4rcel <103891322+d4rcel@users.noreply.github.com>
2025-07-24 23:50:07 +0100

    feat: Set base authentication feature

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,9 +3,9 @@ import {Redirect, Slot} from "expo-router";
 
 const _Layout = () => {
 
-    // const isAuthenticated = true
-    //
-    // if (!isAuthenticated) return <Redirect href={"/sign-in"}/>
+    const isAuthenticated = false
+
+    if (!isAuthenticated) return <Redirect href={"/sign-in"}/>
     return <Slot/>
 }
 export default _Layout
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    platform: "com.fastfood.ordering",
+    databaseId: "6882a27b0005abd287e3",
+    userCollectionId: "6882a2b60000dbb0d69a"
+}
