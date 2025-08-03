ed6fc0813d238a8f4a78986632c6d5c9f7ac57cf
d4rcel <103891322+d4rcel@users.noreply.github.com>
2025-08-03 13:57:49 +0100

    feat: Filter & Search on Search View

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,12 +1,17 @@
 import {Account, Avatars, Client, Databases, ID, Query, Storage} from "react-native-appwrite";
-import {CreateUserParams, SignInParams} from "@/type";
+import {CreateUserParams, GetMenuParams, SignInParams} from "@/type";
 
 export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
     platform: "com.fastfood.ordering",
     databaseId: "6882a27b0005abd287e3",
-    userCollectionId: "6882a2b60000dbb0d69a"
+    bucketId: '688d1464001a0721a4e9',
+    userCollectionId: "6882a2b60000dbb0d69a",
+    categoriesCollectionId: "688ce0ba001dc94c137d",
+    menuCollectionId: '688ce1a50030229fd4d0',
+    customizationsCollectionId: '688ce34f0014e07a98bc',
+    menuCustomizationsCollectionId: '688ce4f0002b6a38a779'
 }
 
 export const client = new Client();
@@ -65,6 +70,38 @@ export const getCurrentUser = async () => {
         return currentUser.documents[0];
     } catch (e) {
         console.log(e);
+        throw new Error(e as string);
+    }
+}
+
+export const getMenu = async ({ category, query }: GetMenuParams) => {
+    try {
+        const queries: string[] = [];
+
+        if(category) queries.push(Query.equal('categories', category));
+        if(query) queries.push(Query.search('name', query));
+
+        const menus = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.menuCollectionId,
+            queries,
+        )
+
+        return menus.documents;
+    } catch (e) {
+        throw new Error(e as string);
+    }
+}
+
+export const getCategories = async () => {
+    try {
+        const categories = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.categoriesCollectionId,
+        )
+
+        return categories.documents;
+    } catch (e) {
         throw new Error(e as string);
     }
 }
